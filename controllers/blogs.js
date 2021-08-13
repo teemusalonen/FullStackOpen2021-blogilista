6856946485de54f5de55a0326cd29e7ddfd91db4
tpsalo <tpsalo@utu.fi>
2021-08-13 16:37:14 +0300

    Tehtävä 4.14 tehty, toimii restclientillä testattuna

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const { response } = require('express')
+const { request } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -18,5 +19,16 @@ blogsRouter.delete('/:id', async (req, res) => {
   await Blog.findByIdAndRemove(req.params.id)
   res.status(204).end()
 })
+
+blogsRouter.put('/:id', async (req, res) => {
+  const newBlog = {
+    likes: req.body.likes
+  }
+  
+  const updated = await Blog.findByIdAndUpdate(req.params.id, newBlog, { new: true })
+  
+  res.json(updated.toJSON())
+})
+
 
 module.exports = blogsRouter
