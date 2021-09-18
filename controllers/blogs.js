082a4985ce64d179c0d69e33740ab29583af2434
tpsalo <tpsalo@utu.fi>
2021-09-18 19:40:54 +0300

    Tehtävä 4.17 done, samoin 4.18?

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,47 @@
 const blogsRouter = require('express').Router()
-const { response } = require('express')
-const { request } = require('../app')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+      .find({})
+      .populate('user', { username: 1, name: 1 })
     response.json(blogs.map(b => b.toJSON()))
 })
-  
+
+const getTokenFrom = request => {  
+  const authorization = request.get('authorization')  
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {    
+    return authorization.substring(7)  
+  }  
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+  const body = request.body
+  
+  const token = getTokenFrom(request)  
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {    
+    return response.status(401).json({ error: 'token missing or invalid' })  
+  }  
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    user: user._id,
+    url: body.url,
+    likes: body.likes ? body.likes : 0 
+  })
     
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog.toJSON())
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)  
+  await user.save()
+
+  response.status(201).json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-  username: String,
-  name: String,
-  passwordHash: String,
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Blog'
     }
   ],
+  username: { type: String, unique: true },
+  name: String,
+  passwordHash: { type: String, required: true },
 })
 
 userSchema.set('toJSON', {
@@ -17,10 +18,11 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
+
+userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
