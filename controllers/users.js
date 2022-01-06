0fd52c0ea31a9dad709d5b8e9f94855b38d1d93d
teemusalonen <teemu.p.salonen@gmail.com>
2022-01-06 18:27:29 +0200

    cleaned up the code

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,31 @@
-const bcrypt = require('bcryptjs')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcryptjs");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User
-    .find({})
-    .populate('blogs')
-  console.log('get :DD:DD:DD:DD:DD')
-  response.json(users.map(u => u.toJSON()))
-})
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs");
+  response.json(users.map((u) => u.toJSON()));
+});
 
-usersRouter.post('/', async (request, response) => {
-  const body = request.body
-  //const users = await User.find({})
-  
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+usersRouter.post("/", async (request, response) => {
+  const body = request.body;
 
-  /*console.log('body.username.length', body.username.length)
-  console.log('users...', (users.find(u => u.username === body.username) === undefined))
-  console.log('body.password', body.password)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-  if(body.username.length < 3 | !(users.find(u => u.username === body.username) === undefined) | !body.password){
-    return response.status(400).json({ error: 'invalid user credentials' })
-  }*/
-
-  if(!body.username || !body.password || body.password.length < 3){
-    return response.status(400).json({ error: 'invalid user information' })
+  if (!body.username || !body.password || body.password.length < 3) {
+    return response.status(400).json({ error: "invalid user information" });
   }
-
 
   const user = new User({
     username: body.username,
     name: body.name,
     passwordHash,
-  })
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.json(savedUser)
-})
+  response.json(savedUser);
+});
 
-module.exports = usersRouter
+module.exports = usersRouter;
